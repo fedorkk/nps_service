a7c69cec83b632152815fa263ff9c68d676d5c89
Fedor Koshel <fedorkk@gmail.com>
2020-01-25 23:21:53 +0100

    remove JwtParams attributes

--- app/services/jwt_params.rb
+++ app/services/jwt_params.rb
@@ -4,14 +4,16 @@ class JwtParams
   SECRET = ENV.fetch('HMAC_SECRET')
   ATTRIBUTES = %i[score touchpoint respondent_class respondent_id object_class object_id].freeze
 
-  attr_reader *ATTRIBUTES
   attr_reader :payload
 
   def initialize(token)
     @payload = JWT.decode(token, SECRET, true).first.symbolize_keys
 
+    # The fetch method raises the KeyError exceprion if the key doesn't exist.
+    # It's a simples check that all necessary fields are existed in the payload, which means that it's valid for our case.
+    # In a more complex situation we can use anoter instrument like dry-struckt
     ATTRIBUTES.each do |attribute|
-      self.instance_variable_set("@#{attribute}", @payload.fetch(attribute))
+      @payload.fetch(attribute)
     end
 
     @valid = true
